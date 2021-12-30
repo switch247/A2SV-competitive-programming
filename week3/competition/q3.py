3bbf5938a2ae0ab87e50c8ef425f628982f9ca80
switch247 <95418375+switch247@users.noreply.github.com>
2021-12-30 22:52:15 +0300

    Create q3.py

---
+++ week3/competition/q3.py
@@ -0,0 +1,11 @@
+#16min
+n=3
+for i in range(n):
+    for j in range(i):
+        print('#'*(n-1),end=" ")
+    print
+print()
+for i in range(n):
+    for j in range(i):
+        print('#'*(n-1),end=" ")
+    print
