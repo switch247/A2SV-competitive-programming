373b6cbb4aee529f3ce069121dc412d773bbd511
switch247 <95418375+switch247@users.noreply.github.com>
2021-12-31 17:28:23 +0300

    Update q3.py

--- week3/competition/q3.py
+++ week3/competition/q3.py
@@ -1,11 +1,9 @@
 #16min
 n=3
-for i in range(n):
-    for j in range(i):
-        print('#'*(n-1),end=" ")
-    print
-print()
-for i in range(n):
-    for j in range(i):
-        print('#'*(n-1),end=" ")
-    print
+for m in range(2):
+    for i in range(n):
+        for j in range(i):
+            print('#'*(n-1),end=" ")
+        print
+    print()
+
