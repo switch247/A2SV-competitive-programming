b493c570f309de79be77128c8c123f2211394a26
switch247 <95418375+switch247@users.noreply.github.com>
2022-01-05 22:34:52 +0300

    Create square_of_sorted_array.py

---
+++ remote_time_questions/square_of_sorted_array.py
@@ -0,0 +1,7 @@
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        for i in range(len(nums)):
+            nums[i]**=2
+        nums.sort()
+        return(nums)
+        
