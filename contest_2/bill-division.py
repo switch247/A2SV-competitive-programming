e4eb0717b0196f0a0ca8a347752191995d1d336e
switch247 <95418375+switch247@users.noreply.github.com>
2022-01-23 16:59:02 +0300

    Create bill-division.py

---
+++ contest_2/bill-division.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'bonAppetit' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY bill
+#  2. INTEGER k
+#  3. INTEGER b
+#
+
+def bonAppetit(bill, k, b):
+    # Write your code here
+    bill.pop(k)
+    ac= (sum(bill)) /2
+    bon = b - ac
+    if b == ac:
+        print("Bon Appetit")
+    else:
+        print ( int(bon) )
+
+if __name__ == '__main__':
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    bill = list(map(int, input().rstrip().split()))
+
+    b = int(input().strip())
+
+    bonAppetit(bill, k, b)
