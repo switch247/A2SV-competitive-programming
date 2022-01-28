78758ab4db12a3dfccb160ef3c1f281ead671220
switch247 <95418375+switch247@users.noreply.github.com>
2022-01-28 21:31:55 +0300

    Update selection.py

--- week2/selection.py
+++ week2/selection.py
@@ -6,3 +6,40 @@ for i in range(len(a)-1):#switches current element with minimum from innner loop
             min = j
     a[i], a[min] = a[min], a[i]
 print(*a)
+
+
+'''#using class and functions to be fancy(this didn't need to be complicated)
+#User function Template for python3
+
+class Solution: 
+    def select(self, arr, i):
+        # code here 
+        return min(arr[i:])
+    
+    def selectionSort(self, arr,n):
+        #code here
+        for i in range(n):
+            selected = self.select(arr, i)#gets minimum from select function
+            selected_pos = arr[i:].index(selected) + i
+            
+            temp = arr[i]
+            arr[i] = selected
+            
+            arr[selected_pos] = temp
+
+#{ 
+#  Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__': 
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        arr = list(map(int, input().strip().split()))
+        Solution().selectionSort(arr, n)
+        for i in range(n):
+            print(arr[i],end=" ")
+        print()
+# } Driver Code Ends
+
+'''
