06a9d0da6fe1027d11db1e955f1e7c1a7d63fa77
switch247 <95418375+switch247@users.noreply.github.com>
2022-01-31 21:09:23 +0300

    Create valid_parenthesis.py

---
+++ stack/valid_parenthesis.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack=[]
+        d={ '}':'{', ')':'(', ']':'[' }
+        for i in s:
+            if i  in d: #if it is a closser
+                if stack and stack[-1]==d[i]:#stack not empty and last ele in stack is =d[i] (then)
+                    stack.pop()
+                else:
+                    return False
+            else:#opener
+                stack.append(i)
+        return True if not stack else False
+     #i fit is an openaer it puts it into a stack,if it is a closer and the stack isnt empty...
