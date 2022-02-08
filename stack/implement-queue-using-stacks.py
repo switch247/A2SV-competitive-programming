5ba6a16db6eca593813371b1e9ee7466f5419616
switch247 <95418375+switch247@users.noreply.github.com>
2022-02-08 16:17:21 +0300

    Update implement-queue-using-stacks.py

--- stack/implement-queue-using-stacks.py
+++ stack/implement-queue-using-stacks.py
@@ -23,3 +23,36 @@ class MyQueue:
 # param_2 = obj.pop()
 # param_3 = obj.peek()
 # param_4 = obj.empty()
+  '''class MyQueue:
+
+    def __init__(self):
+        # Initializing two Stack. stack1 and stack2
+        self.stack1 = []
+        self.stack2 = [] # fill this by poping from stalk1 to stack2[-1]==stlk1[0]
+
+    def push(self, x: int) -> None:
+        # Appending the value to stack1
+        self.stack1.append(x)
+
+    def pop(self) -> int:
+        """Since in stack we can only pop element from last. We first pop all the element from stack1 and append it to stack2. After that we pop the last element from stack2"""
+        
+        if not self.stack2:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+        return self.stack2.pop()
+
+    def peek(self) -> int:
+        # Same expaination as pop
+        
+        if not self.stack2:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+        return self.stack2[-1]
+
+    def empty(self) -> bool:
+        # Check the length of both the stack and return the answer.
+        return len(self.stack1) == 0 and len(self.stack2) == 0
+  
+  
+  '''
