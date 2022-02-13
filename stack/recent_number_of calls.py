800f07d7185168a35943e5fbd6dd5e6706871493
switch247 <95418375+switch247@users.noreply.github.com>
2022-02-13 23:50:55 +0300

    Create recent_number_of calls.py

---
+++ stack/recent_number_of calls.py
@@ -0,0 +1,11 @@
+class RecentCounter:
+    def __init__(self):
+        self.recentRequests=[]
+
+    def ping(self, t: int) -> int:
+        self.recentRequests.append(t)
+        self.recentRequests.reverse()
+        while self.recentRequests[-1]<t-3000:
+            self.recentRequests.pop()
+        self.recentRequests.reverse() #why
+        return len(self.recentRequests)
