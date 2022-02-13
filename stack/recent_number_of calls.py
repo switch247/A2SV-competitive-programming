3500023ffa59325668ba0ab60af31cfcd9228a7a
switch247 <95418375+switch247@users.noreply.github.com>
2022-02-14 00:00:23 +0300

    Update recent_number_of calls.py

--- stack/recent_number_of calls.py
+++ stack/recent_number_of calls.py
@@ -1,5 +1,15 @@
 class RecentCounter:
-    def __init__(self):
+     def __init__(self):
+        self.requests = deque()
+ 
+    def ping(self, t: int) -> int:
+        self.requests.append(t)
+        #count = 0
+        while self.requests[0] < t - 3000:
+            self.requests.popleft()
+        return len(self.requests)
+    
+    '''def __init__(self):
         self.recentRequests=[]
 
     def ping(self, t: int) -> int:
@@ -8,4 +18,4 @@ class RecentCounter:
         while self.recentRequests[-1]<t-3000:
             self.recentRequests.pop()
         self.recentRequests.reverse() #why
-        return len(self.recentRequests)
+        return len(self.recentRequests)'''
