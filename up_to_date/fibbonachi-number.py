5af613e213a6af1d29cf57ad0c1c1832c3f04ef1
switch247 <95418375+switch247@users.noreply.github.com>
2022-12-11 19:04:19 +0300

    Update fibbonachi-number.py

--- up_to_date/fibbonachi-number.py
+++ up_to_date/fibbonachi-number.py
@@ -5,3 +5,23 @@ class Solution: #very slow
         else: return  self.fib(n-1) + self.fib(n-2)
 
         
+ """
+ #very fast but takes alot of space
+class Solution:
+    def __init__(self):
+        self.memo = {}
+        # memo{qustion:answer}
+    
+    def fib(self, N):
+        return self.helper(N)
+    def helper(self,N):
+        if N == 0 or N==1: 
+            return N
+        if N in self.memo:
+            return self.memo[N]
+        
+        self.memo[N] = self.helper(N-1) + self.helper(N-2)
+        return self.memo[N]
+
+"""
+        
