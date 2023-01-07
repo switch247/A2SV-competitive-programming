0fc152dea9e038890a27d0c74aab011adfa5227f
switch247 <95418375+switch247@users.noreply.github.com>
2023-01-07 22:09:58 +0300

    Create rotate-array.py

---
+++ up_to_date/rotate-array.py
@@ -0,0 +1,38 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # nums.reverse()
+        # nums[:k].reverse()
+        # nums[k:].reverse()
+        
+        k=k%len(nums)  # doesn't work with out (this index out of range) 
+        
+       """
+       k=4
+       len(nums)= 9
+       4%9 =4
+       why is it necessary ?????????? haaaaaaaa
+       
+       """
+        
+        
+        
+        n= len(nums)-1
+        def rev(x,l,r):
+            while(l<r):
+                x[l],x[r] = x[r],x[l]
+                l+=1
+                r-=1
+        
+        rev(nums , 0 ,n)
+        rev(nums , 0 ,k-1)
+        rev(nums , k , n)
+        
+        
+        
+        # swallowed = [i:i+k]
+        # for i in range (len(nums),k):
+        #     swallowed = [i:i+k]
+            
