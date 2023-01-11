437a7ae4ed7b79e670a5cd19c1cdc70e57466498
switch247 <95418375+switch247@users.noreply.github.com>
2023-01-11 21:23:20 +0300

    Create find-pivot-index.py

---
+++ up_to_date/find-pivot-index.py
@@ -0,0 +1,26 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        tot =sum(nums)
+        
+        left=0 
+        for i in range(len(nums)):
+            right= tot-nums[i]-left
+            if left==right:
+                return i
+            left +=nums[i]
+        return -1
+/*
+
+#very slow
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        for a in range( len(nums) ):
+            if sum(nums[:a+1]) == sum(nums[a:]):
+                return a
+        return -1
+
+
+
+*/
+        
+        
