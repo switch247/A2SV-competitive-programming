f0d6ef712bdd2d36bf4d7426f397c7726f0e2a53
switch247 <95418375+switch247@users.noreply.github.com>
2023-01-31 01:41:57 +0300

    Update maximum-points-you-can-obtain-from-cards.py

--- up_to_date/maximum-points-you-can-obtain-from-cards.py
+++ up_to_date/maximum-points-you-can-obtain-from-cards.py
@@ -1,10 +1,11 @@
-class Solution:
+class Solution:#cnt be done recurcively because time limit excede
     def maxScore(self, cardPoints: List[int], k: int) -> int:
-        for i in range(1,len(cardPoints) ):
+        #change to prefix sum form
+        for i in range(1,len(cardPoints) ):#beter than sum(cardpoints[i:i+window])
             cardPoints[i] += cardPoints[i-1]
         tot = cardPoints[-1]
         if k == len(cardPoints): return tot
-        #max score= tot- minsubstring        
+               
         window = len(cardPoints) - k
         print(cardPoints)
         min_sum= cardPoints[window-1] 
@@ -12,5 +13,5 @@ class Solution:
         for i in range(1, len(cardPoints)-window+1):
             # print(cardPoints[i+window-1] -cardPoints[i-1])
             min_sum= min(min_sum, cardPoints[i+window-1] -cardPoints[i-1]  )
-            
+            #max score= total sum - minsubstring sum 
         return cardPoints[-1] - min_sum
