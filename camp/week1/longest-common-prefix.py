becae3010bdc18867ef0365ea773fdd4b5328d19
Abel Bekele <95418375+switch247@users.noreply.github.com>
2023-11-29 19:47:45 +0300

    Added solution for Longest Common Prefix

---
+++ camp/week1/longest-common-prefix.py
@@ -0,0 +1,16 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        ans=""
+        copy = sorted(strs)
+        # print(copy)
+        first = copy[0]
+        last = copy[-1]
+        min_len = min( len(first), len(last) ) 
+        print(min_len)
+        for i in range( min_len ):
+            if( first[i]!=last[i] ):
+                return ans
+            ans+= first[i]
+        return ans 
+
+        
