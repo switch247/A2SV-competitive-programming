afb0e6e3488f07ac36de19923e8b3946f21311cd
Abel Bekele <95418375+switch247@users.noreply.github.com>
2023-12-12 18:45:45 +0300

    Update design-underground-system.py

--- camp/week3/design-underground-system.py
+++ camp/week3/design-underground-system.py
@@ -1,29 +1,39 @@
+# from collections import defaultDict
 class UndergroundSystem:
-    def __init__(self):
-        # Dictionary to store total travel time and count for each (start_station, end_station) pair
-        self.travel_times = {}
 
-        # Dictionary to store check-in information with customer_id as key and (start_station, check_in_time) as value
-        self.check_ins = {}
+    def __init__(self):
+        self.checkIn_dict = defaultdict(lambda:[])
+        # self.checkOut_dict = defaultDict(lambda:[])
+        self.average_dict = defaultdict(lambda:[])
 
     def checkIn(self, id: int, stationName: str, t: int) -> None:
-        self.check_ins[id] = (stationName, t)
+        # checkin {}= (id,station):time
+        self.checkIn_dict[id]  = (stationName,t)
+        # print(self.checkIn_dict.items())
+        # id, entering time
 
     def checkOut(self, id: int, stationName: str, t: int) -> None:
-        start_station, check_in_time = self.check_ins.pop(id)
-        travel = (start_station, stationName)
-        travel_time = t - check_in_time
+        #sometimes exitors log might not be there ignore them
+        # find where they entered 
+        if id in self.checkIn_dict:
+            enter_station, enter_time = self.checkIn_dict[ id ]
+            time = t - enter_time
+            self.average_dict[stationName] = self.average_dict[(enter_station, stationName)].append(time)
+            # print(self.average_dict,"average")
 
-        if travel in self.travel_times:
-            total_time, count = self.travel_times[travel]
-            self.travel_times[travel] = (total_time + travel_time, count + 1)
-        else:
-            self.travel_times[travel] = (travel_time, 1)
 
     def getAverageTime(self, startStation: str, endStation: str) -> float:
-        travel = (startStation, endStation)
-        total_time, count = self.travel_times[travel]
-        return total_time / count
+        t = self.average_dict[(startStation, endStation)]
+        if not t:
+            return 0
+        n = len(t)
+        average = sum (t)/ n
+        return average
+        
+        
+
+        
+
 
 # Your UndergroundSystem object will be instantiated and called as such:
 # obj = UndergroundSystem()
