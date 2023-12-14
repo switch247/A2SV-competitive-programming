05c87153d2e0654427edab26bff509ec00a559ee
Abel Bekele <95418375+switch247@users.noreply.github.com>
2023-12-14 16:38:02 +0300

    Added solution for Card Flipping Game

---
+++ camp/week3/card-flipping-game.py
@@ -0,0 +1,25 @@
+class Solution:
+    def flipgame(self, fronts: List[int], backs: List[int]) -> int:
+        bad = set()
+        n = len(fronts)
+        for i in range(n): #o(n)
+            if (fronts[i] == backs[i]):
+                bad.add(fronts[i])
+        # print(bad)
+        # exclude cards which have the same back and front
+        # because flipping these wont change anything but the rest are  good cards
+        # and find the minimum of the rest
+        mn = float('inf')
+        for top, bottom in zip(fronts,backs): #o(n)
+            if ( not bottom in bad ):
+                mn = min( mn, bottom )
+            if ( not top in bad ):
+                mn = min( mn, top )            
+
+
+        return   0 if mn == float('inf') else mn
+
+    # https://leetcode.com/problems/card-flipping-game/solutions/125810/java-solution-using-hashset-the-description-in-the-problem-really-confuses-me/
+    # for further explanation
+
+    # min (in bottom not in up)
