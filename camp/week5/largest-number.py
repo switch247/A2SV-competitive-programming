cd23e638730fdcc4bc1acc535554bd837f1d3447
Abel Bekele <95418375+switch247@users.noreply.github.com>
2023-12-29 17:23:23 +0300

    Added solution for Largest Number

---
+++ camp/week5/largest-number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                p1=str(nums[i])+str(nums[j])
+                p2=str(nums[j])+str(nums[i])
+                if p1 < p2:
+                    nums[i],nums[j]=nums[j],nums[i]
+        result=""
+        for i in nums:
+            result+=str(i)
+        return str(int(result)) # this removes'0' from '00' 
