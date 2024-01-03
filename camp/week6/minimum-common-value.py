80a66486f5aa8aef51396968d90ed61e6b8d8ee5
Abel Bekele <95418375+switch247@users.noreply.github.com>
2024-01-03 18:20:06 +0300

    Update minimum-common-value.py

--- camp/week6/minimum-common-value.py
+++ camp/week6/minimum-common-value.py
@@ -1,18 +1,13 @@
 class Solution:
     def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
-        n = min(len(nums1),len(nums2))
-        nums11=Counter(nums1)
-        nums22=Counter(nums2)
-        ans=-1
-        if len(nums1)<=len(nums2):
-            for i in range(n):
-                if nums1[i] in nums22:
-                    ans= nums1[i]
-                    break
-        else:
-            for i in range(n):
-                if nums2[i] in nums11:
-                    ans= nums2[i]
-                    break
-        return ans
-            
+        n, m = len(nums1) ,  len(nums2)
+        l, r = 0 , 0
+        while  l < n and r < m:
+            if nums1[l] == nums2[r]:
+                return nums1[l] 
+            elif nums1[l] < nums2[r]:
+                l+=1
+            else:
+                r+=1
+    
+        return -1
