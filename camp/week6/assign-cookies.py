e7cda8c5a38aa8d709d9eb02ca74dbb1db6b0507
Abel Bekele <95418375+switch247@users.noreply.github.com>
2024-01-03 18:31:13 +0300

    Update assign-cookies.py

--- camp/week6/assign-cookies.py
+++ camp/week6/assign-cookies.py
@@ -1,17 +1,14 @@
 class Solution:
     def findContentChildren(self, g: List[int], s: List[int]) -> int:
-        ans = 0
-        s.sort()
         g.sort()
-        z = 0
-        for min_size in g:
-            while z < len(s):
-                if min_size <= s[z]:
-                    ans+=1
-                    z+=1 
-                    break
-                else:
-                    # if z == len(s): break
-                    z+=1 
-            
-        return ans
+        s.sort()
+        l = r = 0
+        ans = 0
+        while l < len(g) and r < len(s):
+            if g[l]<=s[r]:
+                l+=1
+                r+=1
+                ans+=1
+            else:
+                r+=1
+        return  ans 
