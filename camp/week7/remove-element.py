84b13cd4406de23f98c8830c56a83e089daf5b23
Abel Bekele <95418375+switch247@users.noreply.github.com>
2024-01-12 00:53:30 +0300

    Added solution for Remove Element

---
+++ camp/week7/remove-element.py
@@ -0,0 +1,13 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        from typing import List
+        k = 0 
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[k] = nums[i] 
+                k += 1
+        
+        return k
+                
+
+        
