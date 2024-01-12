3c7cfe59cde3260a90f36b1dfc87a3dd085ece4b
Abel Bekele <95418375+switch247@users.noreply.github.com>
2024-01-12 18:00:56 +0300

    Added solution for Maximum Erasure Value

--- camp/week7/maximum-erasure-value.py
+++ camp/week7/maximum-erasure-value.py
@@ -3,11 +3,14 @@ class Solution:
         s = set()
         left = 0
         ans = 0
+        ss= 0
         for right in range( len(nums) ):
             while nums[right] in s:
                 s.discard(nums[left])
+                ss-=nums[left]
                 left+=1
             s.add(nums[right])
+            ss+=nums[right]
             # print(s,sum(s))
-            ans = max(ans, sum(s) )
+            ans = max(ans, ss )
         return ans
