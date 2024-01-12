5e474c3ba08709d89dfdfd85ad846c31491294c2
Abel Bekele <95418375+switch247@users.noreply.github.com>
2024-01-12 19:36:47 +0300

    Added solution for Fruit Into Baskets

---
+++ camp/week7/fruit-into-baskets.py
@@ -0,0 +1,17 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        #find the largest consequtive substring with less than 3types of fruits
+        ans = 0
+        n = len(fruits)
+        right=left=0
+        x={}
+        for right,val in enumerate(fruits):
+            x[val]= x.get(val,0) + 1
+            while len(x) > 2:
+                x[ fruits[left] ] -= 1
+                if x[fruits[left] ] == 0:
+                    del(x[ fruits[left] ])
+                left +=1 
+            ans= max(ans,right - left + 1)
+            
+        return ans
