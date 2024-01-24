d50b0fdb617f3a3ab5fbb2da28c077c2f05539bf
Abel Bekele <95418375+switch247@users.noreply.github.com>
2024-01-24 19:46:49 +0300

    Update palindrome-linked-list.py

--- camp/week9/palindrome-linked-list.py
+++ camp/week9/palindrome-linked-list.py
@@ -12,3 +12,40 @@ class Solution:
             temp = temp.next
         
         return s==s[::-1]
+
+
+
+
+# better aproach reverse half of the linked list
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def isPalindrome(self, head: Optional[ListNode]) -> bool:
+        if head.next==None: return True
+        fast = head
+        slow = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+        # reverse half
+        # 123 321 -> 123 123
+        # print(slow)
+        # print(head)
+
+        prev = None
+        while slow:
+            tmp = slow.next
+            slow.next = prev
+            prev = slow
+            slow = tmp
+        # print(head, slow)
+        # print(prev)
+        while prev:
+            if prev.val != head.val:
+                return False
+            prev = prev.next
+            head = head.next
+        return True
