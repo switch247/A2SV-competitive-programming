9b516886713f8b850593531240a68daba3e7ff90
Abel Bekele <95418375+switch247@users.noreply.github.com>
2024-02-19 17:00:32 +0300

    Update minimum-penalty-for-a-shop.py

--- camp/week10/leetcode/minimum-penalty-for-a-shop.py
+++ camp/week10/leetcode/minimum-penalty-for-a-shop.py
@@ -3,8 +3,12 @@ class Solution:
         max_score = score = 0
         best_hour = -1
 
-        for i, c in enumerate(customers):
-            score += 1 if c == 'Y' else -1
+        for i in range(len(customers)):
+            if customers[i] == 'Y':
+                score += 1
+            else:
+                score -= 1
+                
             if score > max_score:
                 max_score, best_hour = score, i
                 
