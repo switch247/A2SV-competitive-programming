ca3c42d7f62cfbf2e691105a1e74faca6fe8fe14
Abel Bekele <95418375+switch247@users.noreply.github.com>
2024-02-29 15:24:20 +0300

    Add solution for Kth Smallest Element in a BST

--- camp/week13/leetcode/kth-smallest-element-in-a-bst.py
+++ camp/week13/leetcode/kth-smallest-element-in-a-bst.py
@@ -6,15 +6,47 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        def inorder(root):
-            if not root:
-                return []
-            left = inorder(root.left)
-            right = inorder(root.right)
-            ans = []
-            ans.extend(left)
-            ans.append(root.val)
-            ans.extend(right)
-            return ans
+        # ans=[]
+        # def inorder(root):
+        #     if not root:
+        #         return 
+        #     left = inorder(root.left)
+        #     ans.append(root.val)
+        #     right = inorder(root.right)
+        #     return 
         
-        return inorder(root)[k-1]
+        # inorder(root)
+        # return ans[k-1]
+        # ans = None
+        ans = None
+        def inorder(node):
+            if node:
+                # go to max left
+                inorder(node.left)
+                # k int points to 1st smaller even thought its the oposite
+                nonlocal k 
+                k -= 1
+                if k == 0:
+                    nonlocal ans
+                    ans = node.val
+                # go 1 right then max left
+                inorder(node.right)
+            
+        inorder(root)
+        return ans
+
+        # same aproach iteratively
+        # stack = []
+        # curr = root
+
+        # while stack or curr:
+        #     while curr:
+        #         stack.append(curr)
+        #         curr = curr.left
+        #     curr = stack.pop()
+        #     k -= 1
+        #     if k == 0:
+        #         return curr.val
+        #     curr = curr.right
+        # return -1
+
