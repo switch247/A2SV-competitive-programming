82bac285260543fbe844b14d26cdb0c2340b89ac
Abel Bekele <95418375+switch247@users.noreply.github.com>
2024-02-29 10:36:12 +0300

    Update validate-binary-search-tree.py

--- camp/week13/leetcode/validate-binary-search-tree.py
+++ camp/week13/leetcode/validate-binary-search-tree.py
@@ -6,23 +6,36 @@
 #         self.right = right
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
-        def inorder(root):
+        # better approach
+        def help(root, min_val, max_val):
             if not root:
-                return []
-            left = inorder(root.left)
-            right = inorder(root.right)
-            ans = []
-            ans.extend(left)
-            ans.append(root.val)
-            ans.extend(right)
-            
-            return ans
-        
-        arr = inorder(root)
-        for i in range(1,len(arr)):
-            if arr[i] <= arr[i-1]:
+                return True
+            if min_val < root.val < max_val:
+                # nodes on the left can't be greater than their parents 
+                left = help(root.left, min_val,  root.val)
+                # nodes on the right can't be lesser than their parents 
+                right = help(root.right,  root.val, max_val)
+                return left and right 
+            else:
                 return False
-        return True
+        return help(root, float('-inf'), float('inf'))
+        # def inorder(root):
+        #     if not root:
+        #         return []
+        #     left = inorder(root.left)
+        #     right = inorder(root.right)
+        #     ans = []
+        #     ans.extend(left)
+        #     ans.append(root.val)
+        #     ans.extend(right)
+            
+        #     return ans
+        
+        # arr = inorder(root)
+        # for i in range(1,len(arr)):
+        #     if arr[i] <= arr[i-1]:
+        #         return False
+        # return True
 
 
         
