d5b7bd5b29d5eaa470fb020078838e4f561b53d7
Abel Bekele <95418375+switch247@users.noreply.github.com>
2024-02-29 10:49:57 +0300

    Update sum-root-to-leaf-numbers.py

--- camp/week13/leetcode/sum-root-to-leaf-numbers.py
+++ camp/week13/leetcode/sum-root-to-leaf-numbers.py
@@ -6,23 +6,35 @@
 #         self.right = right
 class Solution:
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
-        x = []
-        def trav(root,ans):
-            if not root:
-                return
-            cpy = ans[:]
-            cpy.append(str(root.val))
-            if not root.left and not root.right:
-                # leaf
-                x.append(cpy)
-            else:
-                l = trav(root.left,cpy)
-                r = trav(root.right,cpy)
-        
-        trav(root,[])
-        sum = 0
-        for i in range(0, len(x)):
-            sum+= int(''.join(x[i]))
+        # better approach
+        def help(node, val):
+            if not node:
+                return 0
+            val = val*10 + node.val
+            if not (node.left or node.right):
+                # leaf 
+                return val  
 
-        # print(x, sep='\n')
-        return sum
+            # 485 + 491 |+ 40 
+            return help(node.left, val) + help(node.right, val)
+        return help(root, 0)
+        # x = []
+        # def trav(root,ans):
+        #     if not root:
+        #         return
+        #     cpy = ans[:]
+        #     cpy.append(str(root.val))
+        #     if not root.left and not root.right:
+        #         # leaf
+        #         x.append(cpy)
+        #     else:
+        #         l = trav(root.left,cpy)
+        #         r = trav(root.right,cpy)
+        
+        # trav(root,[])
+        # sum = 0
+        # for i in range(0, len(x)):
+        #     sum+= int(''.join(x[i]))
+
+        # # print(x, sep='\n')
+        # return sum
