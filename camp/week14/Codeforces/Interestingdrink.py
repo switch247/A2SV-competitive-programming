8b063ad2ace63c1cad318bf5afa1d8e63fa6c61c
Abel Bekele <95418375+switch247@users.noreply.github.com>
2024-03-08 14:23:26 +0300

    Add solution for Interestingdrink

---
+++ camp/week14/Codeforces/Interestingdrink.py
@@ -0,0 +1,26 @@
+n = int(input())
+prices = list(map(int, input().split()))
+prices.sort()
+
+q = int(input())
+def pos (target):
+    l, r = 0, n-1
+    ans = 0
+    while l<=r:
+        mid = (l+r)//2
+        if prices[mid]<=target:
+            ans = mid +1
+            l = mid+1
+        else:
+            r = mid-1
+    return ans
+
+    # tot =  j
+    # for i,price in enumerate(prices):
+    #     if tot < price:
+    #         return i
+    # return n
+
+for i in range(q):
+    z = int(input())
+    print(pos(z))
