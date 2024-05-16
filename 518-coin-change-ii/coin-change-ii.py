8b3ecb4e0dde4999634918aeb822c364ed6bd582
Abel Bekele <95418375+switch247@users.noreply.github.com>
2024-05-16 19:51:26 +0300

    Time: 105 ms (78.50%) | Memory: 16.7 MB (95.08%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -1,17 +1,12 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        n = len(coins)
-        cache = {}
-        def dp(i, _sum):
-            if _sum > amount:
-                return  0
-            if _sum==amount:
-                return 1
-            if i==-1:
-                return 0
-                
-            if (i, _sum) not in cache:
-                cache[(i, _sum)] =  dp( i, _sum+coins[i] ) +  dp(i-1, _sum) 
-            return cache[(i, _sum)] 
-        return dp(n-1, 0) 
-        
+        amounts= [0]* (amount+1)
+        amounts[0] =1
+        for coin in coins:
+            for x in range(coin, amount+1):
+                diff = (x-coin)
+                # diff = ways to reach x
+                # x = coin + diff
+                amounts[x] += amounts[diff]
+            # print(amounts)
+        return amounts[amount]
