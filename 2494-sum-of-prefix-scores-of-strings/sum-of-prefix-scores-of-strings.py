964da3925d2b602ee0efa48d584d7b914bc538d5
Abel Bekele <95418375+switch247@users.noreply.github.com>
2024-05-29 19:35:32 +0300

    Time: 4684 ms (5.48%) | Memory: 434 MB (20.12%) - LeetSync

--- 2494-sum-of-prefix-scores-of-strings/sum-of-prefix-scores-of-strings.py
+++ 2494-sum-of-prefix-scores-of-strings/sum-of-prefix-scores-of-strings.py
@@ -4,6 +4,7 @@ class Trie:
         self.children = [ None for _ in range(26) ]
     
     def insert(self, word: str) -> None:
+        c = 0
         cur = self
         for w in  word:
             x = ord(w) -97
@@ -11,25 +12,34 @@ class Trie:
                 cur.children[x].count+=1
             else:
                 cur.children[x] = Trie()
-            cur =  cur.children[x]    
+            c += cur.children[x].count
+            cur =  cur.children[x]  
+        return c  
 
     def search(self, word: str) -> bool:
         cur = self
         c = 0
-        for w in  word:
+        
+        for i,w in  enumerate(word):
             x = ord(w) -97
+            if cur.children[x].count == 1:
+                return c + ((len(word) - i) if cur else 0)
             c +=cur.children[x].count
             cur =  cur.children[x] 
-
         return c
 
 class Solution:
     def sumPrefixScores(self, words: List[str]) -> List[int]:
-        t = Trie()
-        for w in words:
-            t.insert(w)
         n = len(words)
+        # d = { words[i]:i for i in range(n)}
+        # words.sort()
+        # print(words, d)
         ans = [0]*n
+
+        t = Trie()
+        for w in words: 
+            x = t.insert(w)
+            # ans[d[w]] = x
         for i in range(n):
             ans[i] = t.search(words[i])
         return ans
