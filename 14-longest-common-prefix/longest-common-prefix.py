4c0020ead1f55ee4d98f8ed5d65219428b6f4dc9
Abel Bekele <95418375+switch247@users.noreply.github.com>
2024-07-07 21:42:57 +0300

    Time: 40 ms (42.61%) | Memory: 16.6 MB (65.81%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -6,20 +6,11 @@ class Solution:
         first = copy[0]
         last = copy[-1]
         min_len = min( len(first), len(last) ) 
-        print(min_len)
+        # print(min_len)
         for i in range( min_len ):
             if( first[i]!=last[i] ):
                 return ans
             ans+= first[i]
         return ans 
-"""
-if you see how words are arranged in a word dictionary
-1 abc
-2 abd
-3 abde
-4 bcd
-the max amount of  prefixes the words can have is as large as how many they have in common
-with the word that is farthest away from it
-"""
 
         
