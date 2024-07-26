5f4ec5ffbed31a035ec3edad378cbcecd52a97c0
Abel Bekele <95418375+switch247@users.noreply.github.com>
2024-07-26 11:16:48 +0300

    Time: 366 ms (67.61%) | Memory: 151.1 MB (26.84%) - LeetSync

--- 1-two-sum/two-sum.dart
+++ 1-two-sum/two-sum.dart
@@ -1,13 +1,15 @@
 class Solution {
   List<int> twoSum(List<int> nums, int target) {
     List<int>? ans = [];
-    for (var i = 0; i < nums.length-1; i++){
-        for (var j =i+1; j< nums.length; j++){
-            if (nums[i]+nums[j]== target){
-                ans.add(i);
-                ans.add(j);
-            }
-            }
+    Map<int,dynamic> test = {};
+    for (var i = 0; i < nums.length; i++){
+        var x = target - nums[i];
+        if (test.containsKey(nums[i])){
+            ans.add(i);
+            ans.add(test[nums[i]]);
+        }
+         test[x] = i;
+
     }
     return ans;
 
