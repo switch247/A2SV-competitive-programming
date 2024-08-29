acf2d5474f33a96a21429422394cf2b3caaed473
Abel Bekele <95418375+switch247@users.noreply.github.com>
2024-08-29 22:17:21 +0300

    Time: 51 ms (15.58%) | Memory: 16.7 MB (41.83%) - LeetSync

---
+++ 682-baseball-game/baseball-game.py
@@ -0,0 +1,13 @@
+class Solution:
+    def calPoints(self, operations: List[str]) -> int:
+        stack = []
+        for operation in operations:
+            if operation == '+':
+                stack.append(stack[-1] + stack[-2])
+            elif operation == 'D':
+                stack.append(stack[-1]*2)
+            elif operation == 'C':
+                stack.pop(-1)
+            else:
+                stack.append(int(operation))
+        return sum(stack)
