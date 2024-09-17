b90f563d45ff1f773a140bbd9780d6e51da15641
Abel Bekele <95418375+switch247@users.noreply.github.com>
2024-09-18 00:09:30 +0300

    Time: 1454 ms (21.74%) | Memory: 73.5 MB (5.29%) - LeetSync

---
+++ 1354-find-players-with-zero-or-one-losses/find-players-with-zero-or-one-losses.py
@@ -0,0 +1,25 @@
+from sortedcontainers import SortedList
+class Solution:
+    def findWinners(self, matches: List[List[int]]) -> List[List[int]]:
+        answer = [ [] ,[] ]
+        D = {}
+        players = set()
+        for winner,loser in matches:
+            D[loser] = D.get(loser,0)  + 1
+            players.update([ winner,loser])
+            # print(players)
+
+        # z = SortedList()
+        # z.update(list(players))
+        # 0 loss 1 loss
+        for player in players:
+            # print(player)
+            value =  D.get(player,0)
+            if value==1:
+                answer[1].append(player)
+            elif value == 0:
+                answer[0].append(player)
+        answer[0].sort()
+        answer[1].sort()
+
+        return answer
