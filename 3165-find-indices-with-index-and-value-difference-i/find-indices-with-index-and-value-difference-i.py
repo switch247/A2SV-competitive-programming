7536ecb4994779e52af35d0031332dc4e1dd82da
Abel Bekele <95418375+switch247@users.noreply.github.com>
2024-10-22 19:59:20 +0300

    Time: 5 ms (98.12%) | Memory: 16.7 MB (12.94%) - LeetSync

--- 3165-find-indices-with-index-and-value-difference-i/find-indices-with-index-and-value-difference-i.py
+++ 3165-find-indices-with-index-and-value-difference-i/find-indices-with-index-and-value-difference-i.py
@@ -1,7 +1,15 @@
 class Solution:
     def findIndices(self, nums: List[int], indexDifference: int, valueDifference: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(len(nums)):
-                if abs(nums[i]-nums[j]) >= valueDifference and (abs(i-j)>=indexDifference):
-                    return [i,j]
+        if indexDifference== 0 and valueDifference==0:
+            return [0,0]
+        maps = defaultdict(list)
+        for j in range(len(nums)):
+            cur_val = nums[j]
+            for old_val in maps:
+                if abs(old_val - cur_val) >= valueDifference:
+                    for i in maps[old_val]:
+                        if abs(i - j) >= indexDifference:
+                            return [i,j]
+            maps[cur_val].append(j)
+
         return [-1,-1]
